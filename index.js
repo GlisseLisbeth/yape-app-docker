1ed0c41e53b2f278565034a369c4e3a70da6f542
glisselisbethjm <glisse1986.jm@outlook.com>
2019-06-02 17:35:16 -0500

    update index.js include host and port

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
 const app = require('express')();
 
 app.get('/', (req, res) => {
   res.send('Hellor express!');
 });
 
-app.listen(3000, () => console.log('Server running'));
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
